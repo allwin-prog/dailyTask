c7aec096909e9058e2cdf367ef3f6f7934a2ea61
allwin-prog <allwinallu99@gmail.com>
2022-08-16 20:52:43 +0530

    Initial Commit

---
+++ day-1.py
@@ -0,0 +1,34 @@
+#CODING STANDARDS
+#PRINTING - HELLO WORLD
+from tokenize import Number
+
+
+print("hello world")
+
+# 1) FUNCTIONS
+
+#DEFINING FUNCTION
+def getData():
+    print("Hello world")
+
+
+#DEFINING FUNCTION 
+def getData(a):
+    #printing a variable
+     print("hi"+a)
+#Invoking a function
+getData("a")
+
+#Defining A Class
+class Computer:
+    #Defining a Function / methods
+    def getData():
+        #Printing Hello world
+        print("Hello world")
+
+
+#mail details
+#under subject
+
+    #1) Day 1 
+    #2) register Number
